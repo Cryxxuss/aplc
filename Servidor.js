b47a549f30b57dc8f1d7854f2d4f1ed240104250
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2023-08-26 20:00:54 -0400

    Update Servidor.js

--- Servidor.js
+++ Servidor.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         Servidor
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://discord.com/channels/812049007651717181/833818811714109450
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
-// @grant        none
-// ==/UserScript==
-
 (function() {
     'use strict';
 
