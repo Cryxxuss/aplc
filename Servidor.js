ee4c3fb1d0dd7a24b3fcc0e3d5633a062608d956
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2023-08-31 21:35:40 -0400

    Update Servidor.js

--- Servidor.js
+++ Servidor.js
@@ -266,11 +266,14 @@ function Enviar_task(){
     if(QuizM.includes("2/2") || QuizM.includes("3/3") || QuizM.includes("4/4") || QuizM.includes("5/5") || QuizM.includes("6/6") || QuizM.includes("7/7") || QuizM.includes("8/8") || QuizM.includes("9/9") || QuizM.includes("10/10")||
        QuizM.includes("11/11") || QuizM.includes("12/12") || QuizM.includes("13/13") || QuizM.includes("14/14") || QuizM.includes("15/15") || QuizM.includes("16/16") || QuizM.includes("17/17") || QuizM.includes("18/18") ||
        QuizM.includes("19/19") || QuizM.includes("20/20") || QuizM.includes("21/21") || QuizM.includes("22/22") || QuizM.includes("23/23") || QuizM.includes("24/24") || QuizM.includes("25/25")){
-
        document.getElementsByClassName('submit btn btn-cf-blue ')[0].click();
 
-    }else if(QuizM.includes("Work")){
-
+    }else if(jobTitle.includes("Text Classification - Agreement") || jobTitle.includes("Look At Advertisements")){
+           if(QuizM.includes("4/5") || QuizM.includes("5/6")){
+               document.getElementsByClassName('submit btn btn-cf-blue ')[0].click();
+           }
+    }else{
+      if(QuizM.includes("Work")){
 	 var hijos = document.querySelector("#job_units").childNodes;
     for (var hijo of hijos){
 		if (hijo.tagName =='DIV' && !hijo.classList.contains('form-actions')){
@@ -278,13 +281,8 @@ function Enviar_task(){
 			if (stilo.includes("green")){
 				document.getElementsByClassName('submit btn btn-cf-blue ')[0].click();
             }}
-     }}
-    else if(jobTitle.includes("Text Classification - Agreement") || jobTitle.includes("Look At Advertisements")){
-           if(QuizM.includes("4/5") || QuizM.includes("5/6")){
-               document.getElementsByClassName('submit btn btn-cf-blue ')[0].click();
-           }
-    }
-}
+      }}
+}}
 
 setInterval(enviar_WM,30000);
 function enviar_WM(){
@@ -331,7 +329,4 @@ var AutoFill_Image_Preference=[""];
 var autofill_checkboxes_random=["1,2"];
 }
 })();
-setTimeout(function(){
-location.reload();
-}, 300000);
-console.log("Nuevos cambios");
+console.log("Espero mande Diooosss");
