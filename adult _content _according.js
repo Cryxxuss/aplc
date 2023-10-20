307aa54d7a5c0a4eec3827869f065ddd47971929
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2023-10-20 19:54:01 -0400

    Update adult _content _according.js

--- adult _content _according.js
+++ adult _content _according.js
@@ -435,6 +435,7 @@ let datos= [
 {url:"http://image.issuu.com/230705080522-8ebdafee41ad683ac695da9b3c264bc4/jpg/page_1_thumb_large.jpg",respuesta: false},
 {url:"http://image.issuu.com/230821174646-9d78698fda2b781f461373f6c9124d88/jpg/page_1_thumb_large.jpg",respuesta: false},
 {url:"http://image.issuu.com/230605144835-d4bbb7d7355dec943c9680df216680bd/jpg/page_1_thumb_large.jpg",respuesta: false},
+{url:"http://image.issuu.com/230822123044-609aeca9b01b7a88ae1f26e8558d4f4d/jpg/page_1_thumb_large.jpg",respuesta: false},
 ]
 let vacias = [
 {url:"http://image.issuu.com/230815214608-bf44fb3e47e247930c2a86118f312af6/jpg/page_1_thumb_large.jpg"},
@@ -452,7 +453,6 @@ let vacias = [
 {url:"http://image.issuu.com/230607051834-ce674a20c7d345dd2f28ee31fbcc04e1/jpg/page_1_thumb_large.jpg"},
 {url:"http://image.issuu.com/db8b2d3f56614a8cb9459d83168b4c0a/jpg/page_1_thumb_large.jpg"},
 {url:"http://image.issuu.com/230602054741-2bea611a57afe75876c43ce07148a86b/jpg/page_1_thumb_large.jpg"},
-{url:"http://image.issuu.com/230822123044-609aeca9b01b7a88ae1f26e8558d4f4d/jpg/page_1_thumb_large.jpg"},
 {url:"http://image.issuu.com/230904070018-a75079fdb03e950fab5ddb1491160b94/jpg/page_1_thumb_large.jpg"},
 {url:"http://image.issuu.com/0a0ae6b85a024aafac11c59d97eb39e3/jpg/page_1_thumb_large.jpg"},
 {url:"http://image.issuu.com/230620143831-f8fd50802f0d5aadf096e29aed1a4c47/jpg/page_1_thumb_large.jpg"},
