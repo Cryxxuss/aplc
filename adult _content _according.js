d807581f91bbfcfb1e081a6fadab3246d0766787
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2023-10-22 12:18:21 -0400

    Update adult _content _according.js

--- adult _content _according.js
+++ adult _content _according.js
@@ -458,6 +458,8 @@ let datos= [
 {url:"http://image.issuu.com/230622155733-bd2d9f7347611de0baa80b4e30ab9eb1/jpg/page_1_thumb_large.jpg",respuesta: false},
 ]
 let vacias = [
+{url:"http://image.issuu.com/230619103912-8f753627d79ed17bdb6f06366e0aa2a4/jpg/page_1_thumb_large.jpg"},
+{url:"http://image.issuu.com/221f33e4edddad8cb07a8e9b1709118a/jpg/page_1_thumb_large.jpg"},
 {url:"http://image.issuu.com/230610104513-2d682a8acff060ebcbc89ca6856faf03/jpg/page_1_thumb_large.jpg"},
 {url:"http://image.issuu.com/892853105e304d43b1c511f9e84c1a38/jpg/page_1_thumb_large.jpg"},
 {url:"http://image.issuu.com/88acb5a886f54b4d8a0a8520c218fbb2/jpg/page_1_thumb_large.jpg"},
