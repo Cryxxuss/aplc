0f1b685f99d412df6eb76e85f2fd0e46c0031e7c
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2023-10-27 08:07:33 -0400

    Create product_categorization.js

---
+++ product_categorization.js
@@ -0,0 +1,107 @@
+// ==UserScript==
+// @name         Product Categorization
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  try to take over the world!
+// @author       Juan pepe
+// @match        https://view.appen.io/assignments/*
+// @connect      account.appen.com
+// @connect      view.appen.io
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+setTimeout(function(){fillAnwsers();}, 64000);
+//activar abrir en otra pestaña
+/*let element = document.querySelector('.btn-cf-blue');
+element.setAttribute("formtarget", "_blank");*/
+
+function fillAnwsers(){
+
+    var titleDivapp = document.createElement("div");
+    var titleapp = document.querySelector(".job-title").innerText;
+    titleDivapp.innerHTML = titleapp;
+    var jobTitle = titleDivapp.innerText;
+
+
+if(jobTitle.includes("Product Categorization")){
+
+    var jsawesome = document.querySelectorAll(".cml");
+    jsawesome.forEach(wrapper => {
+        var titleDiv = wrapper.querySelector("div");
+        var p = wrapper.querySelectorAll("p");
+
+		var linkImg = wrapper.querySelectorAll("img")[0].src;
+
+		//var data = processData(linkImg);
+        var data = processData2(linkImg);
+        var data2 = processData1(linkImg);
+
+		if (data.length > 0) {
+
+			titleDiv.style.backgroundColor = "Gold";
+			titleDiv.style.color = "white";
+			titleDiv.scrollIntoView({ block: "center" })
+
+			var id= wrapper.id;
+
+			var iddd=id+"[does_the_predicted_category_match_the_product]";
+			var primero = document.querySelectorAll("[name='"+iddd+"']");
+
+			switch(data[0].respuesta) {
+			  case true:
+				primero[0].checked=1;
+				primero[0].click();
+				break;
+			  case false:
+				primero[1].checked=1;
+				primero[1].click();
+				break;
+			}
+            setTimeout(enviar,1000);
+        }
+        else if(data2.length > 0){
+
+			titleDiv.style.backgroundColor = "blue";
+			titleDiv.style.color = "white";
+			titleDiv.scrollIntoView({ block: "center" })
+
+			var id2= wrapper.id2;
+
+			var iddd2=id2+"[question1]";
+			var primero2 = document.querySelectorAll("[name='"+iddd2+"']");
+
+            setTimeout(enviar,1000);
+        }
+     });
+	}
+}
+
+    function processData2(url) {
+        let data = datos.filter(datos => datos.url == url)
+        return data;
+    }
+    function processData1(url) {
+        let data2 = vacias.filter(vacias => vacias.url == url)
+        return data2;
+    }
+
+	function enviar(){
+var WM = document.createElement("div");
+var titleapppe = document.querySelector(".navbar").innerText;
+WM.innerHTML = titleapppe;
+var QuizMM = WM.innerText;
+        if(QuizMM.includes("Work")){
+        document.getElementsByClassName('submit btn btn-cf-blue')[0].click();
+}}
+
+let datos= [
+{url:"url",respuesta: false},
+]
+let vacias = [
+{url:"url"},
+]
+})();
+console.log("nuevas tqs");
