88a5ada800cc77b3ae20fadc3abd4daf31e02143
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2023-12-20 13:00:17 -0400

    Update Servidor.js

--- Servidor.js
+++ Servidor.js
@@ -422,7 +422,7 @@ setInterval(rocktq,40000);
 
 id="2323516"
 autofill_radios_random=["1"];
-		//setInterval(Enviar, Math.round(Math.random() * (68000 - 65000)) + 65000);
+		setInterval(Enviar, Math.round(Math.random() * (68000 - 65000)) + 65000);
      break;
 
     default:
