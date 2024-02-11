0dc1bca5a88632e2101bedb8c00670e1865d9a0c
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-02-11 14:42:25 -0400

    Update Prueba_tarea.js

--- Prueba_tarea.js
+++ Prueba_tarea.js
@@ -133,7 +133,7 @@ let datos= [
 {url:"https://storage.cloud.google.com/ai-vision/team/data/eproduct-v2/vision-eproduct-v2/index/GGAAAOSwdjJjvYzP.jpg",respuesta: false},
 {url:"https://storage.cloud.google.com/ai-vision/team/data/eproduct-v2/vision-eproduct-v2/index/ih0AAOSw-~JlASaM.jpg",respuesta: false},
 {url:"https://storage.cloud.google.com/ai-vision/team/data/eproduct-v2/vision-eproduct-v2/index/~08AAOSw7l9h7J7z.jpg",respuesta: false},
-{url:"",respuesta: false},
+{url:"https://storage.cloud.google.com/ai-vision/team/data/eproduct-v2/vision-eproduct-v2/index/SWoAAOSwWWljSK84.jpg",respuesta: false},
 {url:"",respuesta: false},
 {url:"",respuesta: false},
 {url:"",respuesta: false},
