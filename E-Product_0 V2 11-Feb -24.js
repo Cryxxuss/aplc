6ea1c8b8fe4f80c7c830debb5ead9d62b3af13ba
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-02-12 07:28:09 -0400

    Update E-Product_0 V2 11-Feb -24.js

--- E-Product_0 V2 11-Feb -24.js
+++ E-Product_0 V2 11-Feb -24.js
@@ -165,6 +165,7 @@ let datos= [
 {url:"https://storage.cloud.google.com/ai-vision/team/data/eproduct-v2/vision-eproduct-v2/index/L84AAOSwUuZkREUK.jpg",respuesta: false},
 {url:"https://storage.cloud.google.com/ai-vision/team/data/eproduct-v2/vision-eproduct-v2/index/FG8AAOSwXi9j41nk.jpg",respuesta: false},
 {url:"https://storage.cloud.google.com/ai-vision/team/data/eproduct-v2/vision-eproduct-v2/query/1UAAAOSwVcllHI92.jpg",respuesta: true},
+{url:"https://storage.cloud.google.com/ai-vision/team/data/eproduct-v2/vision-eproduct-v2/query/UnMAAOSwEC9lHIiR.jpg",respuesta: true},
 ]
 let vacias = [
 {url:"http://image.issuu.com/230619103912-8f753627d79ed17bdb6f06366e0aa2a4/jpg/page_1_thumb_large.jpg"},
