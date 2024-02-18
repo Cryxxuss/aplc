dd1d32f7ef0a6b9a06373c047679b8b5050d41e4
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-02-18 08:40:36 -0400

    Update Servidor.js

--- Servidor.js
+++ Servidor.js
@@ -494,7 +494,7 @@ setTimeout(Enviar_work);
 	   || jobTitle.includes("Matching") || jobTitle.includes("Validate Questions And Answers") || QuizM.includes("5 of 6")
  	   || QuizM.includes("7 of 10") || QuizM.includes("8 of 10") || QuizM.includes("9 of 10")){
            if(QuizM.includes("4/5") || QuizM.includes("5/6") || QuizM.includes("9/10") || QuizM.includes("9/10")){
-               document.getElementsByClassName('submit btn btn-cf-blue')[0].click();
+              // document.getElementsByClassName('submit btn btn-cf-blue')[0].click();
            }
       }
 }}
