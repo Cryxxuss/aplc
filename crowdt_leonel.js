c2d46b1eb0539a3831bc88a67a8432281559fe00
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-03-20 16:32:26 -0700

    Create crowdt_leonel.js

---
+++ crowdt_leonel.js
@@ -0,0 +1,7 @@
+var QM = document.createElement("div");
+var titleappp = document.querySelector(".mt-8").innerText;
+QM.innerHTML = titleappp;
+var QuizM = QM.innerText;
+    if(QuizM.includes("Your views about gun control")){
+window.location="https://crowdtap.com/dashboard";
+}
