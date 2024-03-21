eaa7c72404a649fdb5a252a6848d00710b53631b
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-03-21 14:47:09 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -2,6 +2,6 @@ var QM = document.createElement("div");
 var titleappp = document.querySelector(".mt-8").innerText;
 QM.innerHTML = titleappp;
 var QuizM = QM.innerText;
-    if(QuizM.includes("Your views about gun control")){
+    if(QuizM.includes("Social Media Poll")){
 window.location="https://crowdtap.com/dashboard";
 }
