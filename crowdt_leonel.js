5a59bf6063d60693c595ed4ac9c6778141d0e030
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-03-27 13:55:33 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -3,5 +3,5 @@ var titleappp = document.querySelector(".mt-8").innerText;
 QM.innerHTML = titleappp;
 var QuizM = QM.innerText;
     if(QuizM.includes("Social Media Poll")){
-window.location="https://crowdtap.com/dashboard";
+//window.location="https://crowdtap.com/dashboard";
 }
