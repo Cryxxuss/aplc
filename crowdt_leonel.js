b6ceba230ef9ad3438ba30cdbd514c80302edd2b
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-03-28 14:30:32 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -1,7 +1,7 @@
-/*var QM = document.createElement("div");
+var QM = document.createElement("div");
 var titleappp = document.querySelector(".mt-8").innerText;
 QM.innerHTML = titleappp;
 var QuizM = QM.innerText;
     if(QuizM.includes("Social Media Poll")){
 window.location="https://crowdtap.com/dashboard";
-}*/
+}
