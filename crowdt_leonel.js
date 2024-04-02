4f15ea91d45963de86cc68a254e16bcbe31dcf61
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-02 16:39:25 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -10,6 +10,6 @@ window.document.title = document.querySelector("div > nav > ul > li.ng-tns-c12-1
 //setTimeout(function displayDate(){
 
 //window.document.title = document.querySelector("div > nav > ul > li.ng-tns-c12-1.points.rounded-3xl.coin-item.sm\\:mr-6.relative.progress-2 > div > span").innerText;
-setTimeout(displayDate,120000);
+//setTimeout(displayDate,120000);
 
-},3000);
+//},3000);
