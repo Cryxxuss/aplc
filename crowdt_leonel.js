a850cc99a4ec9397d56439d4e57ab7150ecbdb4a
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-02 15:51:27 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -8,7 +8,7 @@ window.location="https://crowdtap.com/dashboard";
 
 setTimeout(function displayDate(){
 
-window.document.title = document.querySelector("div > nav > ul > li.ng-tns-c12-1.points.rounded-3xl.coin-item.sm\\:mr-6.relative.progress-2 > div > span").innerText;
+//window.document.title = document.querySelector("div > nav > ul > li.ng-tns-c12-1.points.rounded-3xl.coin-item.sm\\:mr-6.relative.progress-2 > div > span").innerText;
 setTimeout(displayDate,120000);
 
 },3000);
