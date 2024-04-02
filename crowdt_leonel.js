4d200d3af0a9794de5218cc8007b8188024ff4c6
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-02 16:44:12 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -1,3 +1,4 @@
+window.document.title = document.querySelector("div > nav > ul > li.ng-tns-c12-1.points.rounded-3xl.coin-item.sm\\:mr-6.relative.progress-2 > div > span").innerText;
 var QM = document.createElement("div");
 var titleappp = document.querySelector(".mt-8").innerText;
 QM.innerHTML = titleappp;
@@ -5,7 +6,6 @@ var QuizM = QM.innerText;
     if(QuizM.includes("Social Media Poll")){
 window.location="https://crowdtap.com/dashboard";
 }
-window.document.title = document.querySelector("div > nav > ul > li.ng-tns-c12-1.points.rounded-3xl.coin-item.sm\\:mr-6.relative.progress-2 > div > span").innerText;
 
 //setTimeout(function displayDate(){
 
