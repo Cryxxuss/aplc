972ba7ca6c92895a7e764bf75383c6e96aff044c
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-02 17:59:29 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -1,4 +1,4 @@
-window.document.title = document.querySelector("div > nav > ul > li.ng-tns-c12-1.points.rounded-3xl.coin-item.sm\\:mr-6.relative.progress-2 > div > span").innerText;
+window.document.title = document.querySelector("div > nav > ul > li > div > span").innerText;
 var QM = document.createElement("div");
 var titleappp = document.querySelector(".mt-8").innerText;
 QM.innerHTML = titleappp;
