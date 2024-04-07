6418e6c8717748b731dbaa64c5b24d6137d7ad45
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-07 16:14:27 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -1,4 +1,3 @@
-window.location="https://outlook.live.com/mail/0/?nlp=1&RpsCsrfState=d5a90eed-33d2-1318-98d8-e4954b57abe6&url=%2fowa%2f0%2f%3fnlp%253d1%2526RpsCsrfState%253dd5a90eed-33d2-1318-98d8-e4954b57abe6";
 window.document.title = document.querySelector("div > nav > ul > li > div > span").innerText;
 var QM = document.createElement("div");
 var titleappp = document.querySelector(".mt-8").innerText;
