d0a52df7d09bf7f9bfebee823e4d2bfa9b24ef1b
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-23 19:33:44 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -17,7 +17,7 @@ var selectedLine9 = lines[9];
 var selectedLine10 = lines[10];
 var selectedLine11 = lines[11];
 
-const name_task = "et's get to know you!"
+const name_task = "Let's get to know you!"
 setTimeout( function(){
     if(ventana){
 
