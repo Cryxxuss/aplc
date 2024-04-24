ac0a45042cba5ea91db8f3e7209ee1764ad4c9db
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-24 14:25:01 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -17,7 +17,7 @@ var selectedLine9 = lines[9];
 var selectedLine10 = lines[10];
 var selectedLine11 = lines[11];
 
-const name_task = "Let's talk about nuts!"
+const name_task = "Plant-based Food Products at Costco"
 setTimeout( function(){
     if(ventana){
 
