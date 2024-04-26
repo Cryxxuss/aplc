844aa96e8f817d783ec007210ec06b816fe09bd6
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-25 21:27:04 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -22,7 +22,7 @@ var selectedLine9 = lines[9];
 var selectedLine10 = lines[10];
 var selectedLine11 = lines[11];
 
-const name_task = "Where you shop and what you would bu"
+const name_task = "Shopping Habits Survey"
 setTimeout( function(){
     if(ventana){
 
