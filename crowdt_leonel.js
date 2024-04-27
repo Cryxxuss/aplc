fd7d8107e6b0d21f169afd7b681c40e4f5c17603
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-26 17:22:41 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -22,7 +22,7 @@ var selectedLine9 = lines[9];
 var selectedLine10 = lines[10];
 var selectedLine11 = lines[11];
 
-const name_task = "Let's talk about the Future"
+const name_task = "Let’s talk hair care!"
 setTimeout( function(){
     if(ventana){
 
