00279c572f1a28617e157a4de31d05ac5ed5a35a
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-04-29 12:49:43 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -22,7 +22,7 @@ var selectedLine9 = lines[9];
 var selectedLine10 = lines[10];
 var selectedLine11 = lines[11];
 
-const name_task = "Tell us about your beauty routine!"
+const name_task = "WWE and mobile games"
 setTimeout( function(){
     if(ventana){
 
