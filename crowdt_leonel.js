4a5872c5ff231db497a55bba1039db2600018ada
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-05-01 15:46:54 -0700

    Update crowdt_leonel.js

--- crowdt_leonel.js
+++ crowdt_leonel.js
@@ -23,7 +23,7 @@ var selectedLine9 = lines[9];
 var selectedLine10 = lines[10];
 var selectedLine11 = lines[11];
 
-const name_task = "Video games and WWE"
+const name_task = "Holidays!"
 setTimeout( function(){
     if(ventana){
 
