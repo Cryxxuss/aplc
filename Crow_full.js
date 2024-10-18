44e8fe708f7503882243b3be9eddbc3df4e4643c
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-10-17 20:03:28 -0400

    Update Crow_full.js

--- Crow_full.js
+++ Crow_full.js
@@ -721,6 +721,10 @@ ventana=false
 }
 else if (antes && antes.textContent.includes("Crowdtap 101: Learning More About You!")){
 document.querySelector(".close-modal-btn").click()
+var cerrar = document.querySelector("div > div.head-container > app-action-modal-header > div > div > p")
+if (cerrar && cerrar.textContent.includes("Crowdtap 101: Learning More About You!")){
+document.querySelector("div > div.head-container > app-action-modal-header > div > button").click();
+}
     ventana=true
 
 }},1000)
