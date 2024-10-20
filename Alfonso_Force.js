44bddd6aff32769c6faae105e2d59ef2e4a4c771
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-10-20 15:25:45 -0400

    Update Alfonso_Force.js

--- Alfonso_Force.js
+++ Alfonso_Force.js
@@ -40,7 +40,9 @@ function checkIPChange() {
             console.log('La dirección IP ha cambiado:', newIP);
 var segundoPaso = document.querySelector("#main-content > article > div.center-column > form > div:nth-child(2) > label")
 if(segundoPaso && segundoPaso.textContent.includes("City")){
+setTimeout( function() {
 document.querySelector("#main-content > article > div.center-column > form > section > button.relative.font-bold.text-white.rounded-full.w-68.h-14.bg-blue-dark").click();
+},3000)
 }
             currentIP = newIP;
         } else {
