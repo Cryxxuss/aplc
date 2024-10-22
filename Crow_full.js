0a160714fd92dd43296b2ed2cf6429371380ad06
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-10-21 23:38:26 -0400

    Update Crow_full.js

--- Crow_full.js
+++ Crow_full.js
@@ -832,7 +832,7 @@ var spanElement = document.querySelector("div > div.head-container > app-action-
 if (spanElement) {
 const spanText = document.querySelector("div > div.head-container > app-action-modal-header > div > div > p").innerText
 if (spanText === "Crowdtap 101: Getting Rewarded" || spanText === "Crowdtap 101: Your Membership"
-    || spanText === "Crowdtap 101: Survey Types" || spanText === "Crowdtap 101: Your Crowdtap Habits"
+    || spanText === "Crowdtap 101: Question Types" || spanText === "Crowdtap 101: Your Crowdtap Habits"
     || spanText === "Crowdtap 101: The Basics" ){
 ventana=false
 } else {
