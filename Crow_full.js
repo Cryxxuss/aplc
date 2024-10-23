6760c8b38f6f385e894cab7d340912139ed20a85
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-10-23 19:06:12 -0400

    Update Crow_full.js

--- Crow_full.js
+++ Crow_full.js
@@ -816,8 +816,8 @@ var selectedLine6 = lines[5];
 var selectedLine7 = lines[6];
     if(ventana){
 
-if (selectedLine1 === "Crowdtap 101: The Basics") {
-document.querySelector("#divContainer > div > div:nth-child(1) > app-tile > button > div.ng-star-inserted > span").click()
+if (selectedLine2 === "Crowdtap 101: The Basics") {
+document.querySelector("#divContainer > div > div:nth-child(2) > app-tile > button > div.ng-star-inserted > span").click()
 ventana=false
 }else if (selectedLine2 === "Crowdtap 101: Question Types") {
 document.querySelector("#divContainer > div > div:nth-child(3) > app-tile > button > div.ng-star-inserted > span").click()
