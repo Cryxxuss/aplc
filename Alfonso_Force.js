01a6d8385324562a3e006e4c3aa40c689e2034a0
Cryxxuss <142957060+Cryxxuss@users.noreply.github.com>
2024-10-22 22:29:49 -0400

    Update Alfonso_Force.js

--- Alfonso_Force.js
+++ Alfonso_Force.js
@@ -195,7 +195,7 @@ GM_xmlhttpRequest({
     });
 setTimeout( function() {
 document.querySelector("#main-content > article > div.center-column > form > section > button.relative.font-bold.text-white.rounded-full.w-68.h-14.bg-blue-dark").click();
-},40000)
+},50000)
 
 
 
